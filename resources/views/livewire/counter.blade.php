f5559f82025681b126a7e1fa1481a947ec2223f4
joan <joan.vanduren@gmail.com>
2021-08-11 14:49:58 +0200

    counter component gemaakt

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>Module Laravel2</h1>
+    <h2>Week2</h2>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+Route::get('/', function () {
+    return view('app');
+});
